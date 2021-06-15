026465ddced5dc254f94e8c59583827ff4aa2184
Magueye Gueye <magueye717@gmail.com>
2021-06-15 13:54:56 +0000

    first commit

--- src/Components/CardList.jsx
+++ src/Components/CardList.jsx
@@ -6,7 +6,7 @@ export const CardList = props =>(
             {props.frameworks.map(framework=>(
                 <div className="card">
                 <h2>{framework.name} - {framework.language}</h2>
-                <img src={`/img/${framework.imgTag}.png`} height="60%" width="100%" alt="photo" />
+                <img src={`./img/${framework.imgTag}.png`} height="60%" width="100%" alt="photo" />
                 <p>Catégory : {framework.category}</p>
             </div>
             ))}
